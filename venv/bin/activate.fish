18f67163e6a3cc4e7cfecb118b35c3a2c144e863
root <root@Debian12.apple.com>
2024-08-23 13:59:17 +0000

    test3

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/padoc/venv"
+set -gx VIRTUAL_ENV "/root/github/padoc/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
